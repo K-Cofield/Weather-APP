a508beaaf8e37bf99aeeacc9744c76e79b3a5d69
Kiante Cofield <134896943+K-Cofield@users.noreply.github.com>
2023-12-19 17:29:17 -0500

    Code
    Code for the application

---
+++ main.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+load_dotenv()
+import requests
+import os
+
+def get_weather(api_key, location):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        temperature = data['main']['temp']
+        humidity = data['main']['humidity']
+        description = data['weather'][0]['description']
+        return temperature, humidity, description
+    else:
+        return None, None, None
+
+def main():
+    api_key = os.environ.get('OPENWEATHER_API_KEY')
+    if not api_key:
+        print("API key not found. Please set the OPENWEATHER_API_KEY environment variable.")
+        return
+
+    while True:
+        location = input("Enter a location (or 'exit' to quit): ")
+        if location.lower() == 'exit':
+            break
+        temperature, humidity, description = get_weather(api_key, location)
+        if temperature is not None:
+            print(f"Weather in {location}:")
+            print(f"Temperature: {temperature} °C, Humidity: {humidity}%, Description: {description}")
+        else:
+            print("Error: Location not found or API error.")
+
+if __name__ == '__main__':
+    main()
